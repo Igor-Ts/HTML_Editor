b3d8734cffdc9933536770175ac891ac580c761d
Igor-Ts <igor.tsepulkovski@gmail.com>
2023-03-03 19:44:13 +0200

    Finished task 1. Started MVC model realization. Added Controller and view classes. Added task 2.

---
+++ src/main/Controller.java
@@ -0,0 +1,20 @@
+package main;
+
+import javax.swing.text.html.HTMLDocument;
+import java.io.File;
+
+public class Controller {
+
+    private View view;
+    private HTMLDocument document;
+    private File currentFile;
+
+    public Controller(View view) {
+        this.view = view;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
