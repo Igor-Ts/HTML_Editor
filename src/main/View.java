a724a898807b8efefc07fd17d66a96566e8f3c3d
Igor-Ts <igor.tsepulkovski@gmail.com>
2023-03-04 14:44:58 +0200

    Finished task 3. Added skeleton for two tabs(visualize html editor, html editor as text). Added task 4.

--- src/main/View.java
+++ src/main/View.java
@@ -7,6 +7,9 @@ import java.awt.event.ActionListener;
 public class View extends JFrame implements ActionListener {
 
     private Controller controller;
+    private JTabbedPane tabbedPane; // it's panel with two tabs.
+    private JTextPane htmlTextPane; // first tab. It'll component for visualize html editor.
+    private JEditorPane plainTextPane; // It'll component for html editor as text. He'll show html code(tags).
 
     public Controller getController() {
         return controller;
