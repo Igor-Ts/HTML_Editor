7b7958c26cc994ab5a0f7c624c74d04ec005a689
Igor-Ts <igor.tsepulkovski@gmail.com>
2023-03-09 20:18:27 +0200

    Finished task 17. Added method which get text with HTML tags from document. Added task 18.

--- src/main/Controller.java
+++ src/main/Controller.java
@@ -8,6 +8,7 @@ import javax.swing.text.html.HTMLEditorKit;
 import java.io.File;
 import java.io.IOException;
 import java.io.StringReader;
+import java.io.StringWriter;
 
 public class Controller {
 
@@ -59,6 +60,16 @@ public class Controller {
         } catch (IOException | BadLocationException e) {
             ExceptionHandler.log(e);
         }
+    }
 
+    public String getPlainText() {
+        StringWriter writer = new StringWriter();
+        try {
+            new HTMLEditorKit().write(writer,document,0, document.getLength());
+        } catch (IOException | BadLocationException e) {
+            ExceptionHandler.log(e);
+        }
+
+        return writer.toString();
     }
 }
