9e0b34cf8fb59c7b51d9180b59bb7f422dd076e4
Igor-Ts <igor.tsepulkovski@gmail.com>
2023-03-10 19:32:41 +0200

    Finished task 20. Realized: create new document. Added task 21.

--- src/main/Controller.java
+++ src/main/Controller.java
@@ -32,7 +32,7 @@ public class Controller {
     }
 
     public void init() {
-
+        createNewDocument();
     }
 
     public void exit() {
@@ -75,7 +75,11 @@ public class Controller {
     }
 
     public void createNewDocument() {
-
+        view.selectHtmlTab();
+        resetDocument();
+        view.setTitle("HTML Editor");
+        view.resetUndo();
+        currentFile = null;
     }
 
     public void openDocument() {
